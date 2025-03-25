0898356a2dedbcdb0d1d34bafce5fe51ec94a56e
Christian Torres <Christian.atm24@gmail.com>
2025-03-25 18:20:09 -0300

    modificando la coneccion con pg

--- db.js
+++ db.js
@@ -1,22 +1,4 @@
-// Con coneccion local
-import pg from 'pg';
-const { Pool } = pg;
-
-import dotenv from 'dotenv';
-dotenv.config();
-
-const pool = new Pool({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME
-});
-
-export default pool;
-
-
-// Con los datos de la base de datos en render:
+// // Con coneccion local
 // import pg from 'pg';
 // const { Pool } = pg;
 
@@ -28,18 +10,36 @@ export default pool;
 //     password: process.env.DB_PASSWORD,
 //     host: process.env.DB_HOST,
 //     port: process.env.DB_PORT,
-//     database: process.env.DB_NAME,
-//     ssl: {
-//         rejectUnauthorized: false
-//     },
-//     connectionTimeoutMillis: 5000,
-//     idleTimeoutMillis: 30000,
-//     max: 20
-// });
-
-// pool.on('error', (err) => {
-//     console.error('Unexpected error on idle client', err);
-//     process.exit(-1);
+//     database: process.env.DB_NAME
 // });
 
 // export default pool;
+
+
+// Con los datos de la base de datos en render:
+import pg from 'pg';
+const { Pool } = pg;
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+const pool = new Pool({
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    ssl: {
+        rejectUnauthorized: false
+    },
+    connectionTimeoutMillis: 5000,
+    idleTimeoutMillis: 30000,
+    max: 20
+});
+
+pool.on('error', (err) => {
+    console.error('Unexpected error on idle client', err);
+    process.exit(-1);
+});
+
+export default pool;
